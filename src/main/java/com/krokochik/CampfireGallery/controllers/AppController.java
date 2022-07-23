38729814d7a1f1ca8f254e1e84cb49a3a569fb03
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-07-23 19:30:22 +0300

    first commit

---
+++ src/main/java/com/krokochik/CampfireGallery/controllers/AppController.java
@@ -0,0 +1,25 @@
+package com.krokochik.CampfireGallery.controllers;
+
+import com.google.gson.JsonObject;
+import org.apache.tomcat.util.json.JSONParser;
+import org.apache.tomcat.util.json.ParseException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+@RestController
+public class AppController {
+
+    @PostMapping("/")
+    public Map<String, String> commandsParse(String stringJson) throws ParseException {
+        JsonObject jsonObject = (JsonObject) new JSONParser(stringJson).parse();
+        HashMap<String, String> response = new HashMap<>();
+        response.put("request", stringJson);
+        return response;
+    }
+}
---
+++ src/main/java/com/krokochik/CampfireGallery/veryHardSite.java
@@ -0,0 +1,10 @@
+package com.krokochik.CampfireGallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class veryHardSite {
+
+	public static void main(String[] args) { SpringApplication.run(veryHardSite.class, args); }
+}
