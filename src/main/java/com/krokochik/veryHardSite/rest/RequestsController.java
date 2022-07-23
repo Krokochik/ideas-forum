637f270294b43d50a9ce6638797fa444604023f3
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-07-23 20:09:27 +0300

    commit

--- src/main/java/com/krokochik/veryHardSite/rest/RequestsController.java
+++ src/main/java/com/krokochik/veryHardSite/rest/RequestsController.java
@@ -16,7 +16,7 @@ public class RequestsController {
     public Map<String, String> commandsParse(String stringJson) throws ParseException {
         HashMap<String, String> response = new HashMap<>();
         //try {
-            //Object object = new JSONParser(stringJson).parse();
+            Object object = new JSONParser(stringJson).parse();
             //JsonObject jsonObject = (JsonObject) object;
             //response.put("request", stringJson);
         //} catch (Exception e) {
