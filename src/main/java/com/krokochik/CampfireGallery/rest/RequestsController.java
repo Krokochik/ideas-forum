33c15255489dce84058cdc922a7947b6c4d20695
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-07-23 19:49:26 +0300

    commit

--- src/main/java/com/krokochik/CampfireGallery/rest/RequestsController.java
+++ src/main/java/com/krokochik/CampfireGallery/rest/RequestsController.java
@@ -3,19 +3,16 @@ package com.krokochik.CampfireGallery.rest;
 import com.google.gson.JsonObject;
 import org.apache.tomcat.util.json.JSONParser;
 import org.apache.tomcat.util.json.ParseException;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 @RestController
 public class RequestsController {
 
-    @PostMapping("/")
+    @PostMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, String> commandsParse(String stringJson) throws ParseException {
         JsonObject jsonObject = (JsonObject) new JSONParser(stringJson).parse();
         HashMap<String, String> response = new HashMap<>();
