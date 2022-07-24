33045383818db4de26d9568e773787cd59c520e0
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-07-24 20:36:04 +0300

    commit

--- src/main/java/com/krokochik/CampfireGallery/rest/RequestsController.java
+++ src/main/java/com/krokochik/CampfireGallery/rest/RequestsController.java
@@ -4,16 +4,24 @@ import com.krokochik.CampfireGallery.repository.NumbersRepository;
 import com.krokochik.CampfireGallery.service.ValueManagerService;
 import org.apache.tomcat.util.json.JSONParser;
 import org.apache.tomcat.util.json.ParseException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class RequestsController {
 
-    public final NumbersRepository numbersRepository = new NumbersRepository();
-    public final ValueManagerService valueManagerService = new ValueManagerService();
+    private final NumbersRepository numbersRepository = new NumbersRepository();
+    private final ValueManagerService valueManagerService = new ValueManagerService();
+
+    @ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No such Order")  // 404
+    public class OrderNotFoundException extends RuntimeException {
+        // ...
+    }
 
     @PostMapping(path = "/")
     public Map<String, String> commandsParse(@RequestBody String stringJson) throws ParseException { ;
@@ -37,44 +45,37 @@ public class RequestsController {
     }
 
     @PostMapping("/repositories/{id}")
-    public Map<String, String> repositories(@PathVariable(name = "id") int id, @RequestBody String requestBody) throws ParseException {
+    public Map<String, String> repositories(@PathVariable(name = "id") int id, @RequestBody String requestBody, HttpServletResponse httpServletResponse)
+            throws ParseException, IOException {
         if(valueManagerService.isRepositoryExist(id)) {
             short status = 200;
-            System.out.println(1);
             HashMap<String, String> response = new HashMap<>();
-            System.out.println(2);
             HashMap<String, String> request = (HashMap<String, String>) new JSONParser(requestBody).parse();
-            System.out.println(3);
             switch (request.get("command")) {
                 case "addVariable":
-                    System.out.println("adV");
                     try {
-                        valueManagerService.addVariable(request.get("name"), request.get("value"), id); }
+                        valueManagerService.addVariable(request.get("name"), request.get("value"), id);
+                        status = 201;
+                    }
                     catch (NullPointerException e) { status = 400; }
                     catch (Exception e) { status = 500; }
                     break;
                 case "getVariableValue":
-                    System.out.println("getVV");
                     try {
-                        valueManagerService.getVariable(request.get("name"), id);
-                    } catch (NullPointerException | IndexOutOfBoundsException nullPointerException) {
-                        status = 404;
-                    }
+                        response.put("value", valueManagerService.getVariable(request.get("name"), id)); }
+                    catch (NullPointerException | IndexOutOfBoundsException nullPointerException) { status = 404; }
                     break;
                 case "changeVariableValue":
-                    System.out.println("chVV");
                     try {
-                        valueManagerService.changeVariable(request.get("name"), request.get("newValue"), id);
-                    } catch (NullPointerException nullPointerException) {
-                        status = 400;
-                    }
+                        valueManagerService.changeVariable(request.get("name"), request.get("newValue"), id); }
+                    catch (NullPointerException nullPointerException) { status = 400; }
                     break;
                 default:
-                    System.out.println("df");
                     status = 400;
                     break;
             }
             response.put("status", status + "");
+            httpServletResponse.sendError(404, "test");
             return response;
         } else return new HashMap<>(){{ put("status", "404"); }};
     }
