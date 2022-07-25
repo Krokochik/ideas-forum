82c98e3fa203d43d14bb0b5ae5ee91d6c6d3e940
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-07-25 22:55:49 +0300

    commit

--- src/main/java/com/krokochik/CampfireGallery/security/WebSecurityConfig.java
+++ src/main/java/com/krokochik/CampfireGallery/security/WebSecurityConfig.java
@@ -1,14 +1,9 @@
 package com.krokochik.CampfireGallery.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,25 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                .and()
-                    .cors().disable();
-    }
-
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("siteDefaultUser")
-                        .password("sitePassword")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+                    .antMatchers("/**", "/**/**").permitAll()
+                .anyRequest().authenticated().and().cors().disable();
     }
 }
