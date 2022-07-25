dacb9c57c9e0570debd0ba7d76e7751c25d179a1
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-07-25 04:41:19 +0300

    commit

--- src/main/java/com/krokochik/CampfireGallery/security/WebSecurityConfig.java
+++ src/main/java/com/krokochik/CampfireGallery/security/WebSecurityConfig.java
@@ -4,15 +4,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                     .antMatchers("/**", "/**/**").permitAll()
                 .anyRequest().authenticated();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://ap-plication.herokuapp.com")
+                .allowedMethods("*");
     }
 }
