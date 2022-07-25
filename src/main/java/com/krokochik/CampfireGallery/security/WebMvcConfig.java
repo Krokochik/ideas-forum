901ccf9cee55737806e945309453fb0426d92be2
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-07-25 23:16:18 +0300

    commit

--- src/main/java/com/krokochik/CampfireGallery/security/WebMvcConfig.java
+++ src/main/java/com/krokochik/CampfireGallery/security/WebMvcConfig.java
@@ -1,26 +1,23 @@
 package com.krokochik.CampfireGallery.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer{
+public class WebMvcConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/**", "/**/**").permitAll()
-                .anyRequest().authenticated().and().cors();
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("*");
+                .antMatchers("/**", "/**/**").permitAll()
+                .anyRequest().authenticated();
     }
 }
