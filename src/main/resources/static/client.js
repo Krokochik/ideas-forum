608d6368e49b8f2f990b2e9204646214ad79e44c
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-07-30 19:47:40 +0300

    commit

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -4,12 +4,17 @@ let minV = document.getElementById('minValue').value;
 let maxV = document.getElementById('maxValue').value;
 
 function generate() {
-    let repeats = document.getElementById('repeats').value;
-    numbers.length = 0;
-    minV = document.getElementById('minValue').value;
-    maxV = document.getElementById('maxValue').value;
-    document.getElementById('numbers').innerHTML = "";
-    send(0, Number(repeats));    
+    if ((document.getElementById('checkbox').checked == true && document.getElementById('repeats').value <= document.getElementById('maxValue').value)
+      || document.getElementById('checkbox').checked == false) { 
+      let repeats = document.getElementById('repeats').value;
+      numbers.length = 0;
+      minV = document.getElementById('minValue').value;
+      maxV = document.getElementById('maxValue').value;
+      document.getElementById('numbers').innerHTML = "";
+      send(0, Number(repeats));  
+    }
+    else 
+    alert("Количество повторов превышает маскимальное значение, невозможно сгенерировать без дубликтов");
 }
 
 function send(i = 0, howMany) {
