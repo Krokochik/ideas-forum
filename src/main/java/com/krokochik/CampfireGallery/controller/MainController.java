191e37de096c45b9f6f5ebd4dd55ce287358cf51
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-08-21 04:25:06 +0300

    commit

--- src/main/java/com/krokochik/CampfireGallery/controller/MainController.java
+++ src/main/java/com/krokochik/CampfireGallery/controller/MainController.java
@@ -6,19 +6,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class MainController {
 
 
     @GetMapping({"/main", "/", ""})
-    public String mainPage(Model model,  HttpServletRequest request, @RequestParam(name = "lang", defaultValue = "", required = false) String language) {
+    public String mainPage(Model model,  HttpServletRequest request, HttpServletResponse response,
+                           @RequestParam(name = "lang", defaultValue = "", required = false) String language,
+                           @RequestParam(name = "theme", defaultValue = "", required = false) String theme) {
+
+        model.addAttribute("theme", theme.equals("light") ? 2 : 1);
         model.addAttribute("lang", !language.equals("") ? language : request.getHeader("Accept-Language").substring(0, 2));
         return "main";
     }
 
     @GetMapping("/add-note")
-    public String addNotePage(Model model,  HttpServletRequest request, @RequestParam(name = "lang", defaultValue = "", required = false) String language) {
+    public String addNotePage(Model model,  HttpServletRequest request, HttpServletResponse response,
+                           @RequestParam(name = "lang", defaultValue = "", required = false) String language,
+                           @RequestParam(name = "theme", defaultValue = "", required = false) String theme) {
+
+        model.addAttribute("theme", theme.equals("light") ? 2 : 1);
         model.addAttribute("lang", !language.equals("") ? language : request.getHeader("Accept-Language").substring(0, 2));
         return "add-note";
     }
