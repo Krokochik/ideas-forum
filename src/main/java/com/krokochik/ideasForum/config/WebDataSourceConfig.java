ad205fb53aac0c9d3ba24b4cb3242a4812d14a73
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-08-23 21:31:55 +0300

    commit

---
+++ src/main/java/com/krokochik/ideasForum/config/WebDataSourceConfig.java
@@ -0,0 +1,4 @@
+package com.krokochik.ideasForum.config;
+
+import org.springframework.context.annotation.Configuration;
+
--- src/main/java/com/krokochik/ideasForum/ideasForum.java
+++ src/main/java/com/krokochik/ideasForum/ideasForum.java
@@ -1,10 +1,13 @@
 package com.krokochik.ideasForum;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }, scanBasePackages={"com.krokochik.ideasForum.controller",
+"com.krokochik.ideasForum.config", "com.krokochik.ideasForum.entity", "com.krokochik.ideasForum", "com.krokochik.ideasForum.repository", "com.krokochik.ideasForum.rest",
+"com.krokochik.ideasForum.service"})
 public class ideasForum {
 	public static void main(String[] args) throws Exception { SpringApplication.run(ideasForum.class, args); }
 }
