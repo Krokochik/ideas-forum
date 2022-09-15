216cd7a621b37b2e3d1f69142e5008bae0a01e58
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-09-16 00:32:20 +0300

    commit

--- src/main/java/com/krokochik/ideasForum/config/WebSecurityConfig.java
+++ src/main/java/com/krokochik/ideasForum/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
                 .antMatchers("/", "/main", "/main/**", "/settings", "/mail-confirm", "/password-abort", "/abortPass", "/pass-abort-notify")
                     .permitAll()
-                .antMatchers("/login", "/sign-up", "/password-abort", "/abortPass")
+                .antMatchers("/login", "/sign-up", "/password-abort")
                     .not().hasAnyAuthority(Role.USER.name(), Role.ADMIN.name(), Role.MODER.name(), Role.ANONYM.name())
                 .antMatchers("/add-note")
                     .hasAnyAuthority(Role.USER.name())
