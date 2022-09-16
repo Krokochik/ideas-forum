638990c317f5bd0c6fcb88bdbc0bf9ba04f0b15b
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-09-16 23:35:30 +0300

    commit

---
+++ src/main/java/com/krokochik/ideasForum/controller/Terminal.java
@@ -0,0 +1,22 @@
+package com.krokochik.ideasForum.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class Terminal {
+
+    @PostMapping("/terminal")
+    public Map<String, Object> commandsMapping(@RequestBody String requestBodyStr) {
+        System.out.println("request");
+        System.out.println(requestBodyStr);
+        return new HashMap<>() {{
+            put("status", "200");
+        }};
+    }
+
+}
--- src/main/java/com/krokochik/ideasForum/controller/TerminalController.java
+++ src/main/java/com/krokochik/ideasForum/controller/TerminalController.java
@@ -2,12 +2,6 @@ package com.krokochik.ideasForum.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class TerminalController {
@@ -17,15 +11,4 @@ public class TerminalController {
         return "terminal";
     }
 
-    @RestController
-    static class Terminal {
-        @PostMapping("/terminal")
-        public Map<String, Object> commandsMapping(@RequestBody String requestBodyStr) {
-            System.out.println("request");
-            System.out.println(requestBodyStr);
-            return new HashMap<>() {{
-                put("status", "200");
-            }};
-        }
-    }
 }
