f1ba13a382ba70f20262bbafc8d5837a502964b7
Krokochik <96812862+Krokochik@users.noreply.github.com>
2022-09-17 22:22:48 +0300

    commit

--- src/main/java/com/krokochik/ideasForum/controller/TerminalLogicController.java
+++ src/main/java/com/krokochik/ideasForum/controller/TerminalLogicController.java
@@ -66,10 +66,8 @@ public class TerminalLogicController {
             put(true, "200" +
                     """
                     Commands: add, del.\r
-                        add: add new field to datasource.
-                            Required params are params from field constructor.\r
-                        del: delete field from datasource.
-                            Required parameters not defined.\r
+                        add: add new field to datasource. Required params are params from field constructor.\r
+                        del: delete field from datasource. Required parameters not defined.\r
                     """);
         }};
     }
