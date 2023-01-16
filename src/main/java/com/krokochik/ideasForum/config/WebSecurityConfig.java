9fc1b33117e1f65ce344ea84da7c64cc92a5fdcf
Krokochik <pasha09braginec@gmail.com>
2023-01-16 23:43:01 +0300

    commit

--- src/main/java/com/krokochik/ideasForum/config/WebSecurityConfig.java
+++ src/main/java/com/krokochik/ideasForum/config/WebSecurityConfig.java
@@ -58,7 +58,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         .useSecureCookie(true)
                 .and()
                     .csrf()
-                        .ignoringAntMatchers("/terminal/**", "/profile", "/sign-up");
+                        .ignoringAntMatchers("/terminal/**", "/profile", "/sign-up")
+                .and().cors();
     }
 
     @Bean
