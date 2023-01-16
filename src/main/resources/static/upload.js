e13f8df064cf9a3b9908b9010affbb0fc512b864
Krokochik <pasha09braginec@gmail.com>
2023-01-17 00:09:42 +0300

    commit

--- src/main/resources/static/upload.js
+++ src/main/resources/static/upload.js
@@ -54,13 +54,13 @@ function saveChanges() {
         freader.readAsDataURL(file);
         freader.onload = (function (f) {
             return function (e) {
-                sendRequest('POST', "https://ideasforum.herokuapp.com/profile", {nickname: newNickname, username: document.getElementById('name').value, avatar: img})
+                sendRequest('POST', "https://ideas-forum.herokuapp.com/profile", {nickname: newNickname, username: document.getElementById('name').value, avatar: img})
                   .then(data => {if ((document.getElementById("nickname").value.trim().length >= 4) || (document.getElementById("imagePreview").style !== document.getElementById("avatar").style))
                                     window.location.href = window.location.href;})
             };
         })(file);
     }
-    else sendRequest('POST', "https://ideasforum.herokuapp.com/profile", {nickname: newNickname, username: document.getElementById('name').value})
+    else sendRequest('POST', "https://ideas-forum.herokuapp.com/profile", {nickname: newNickname, username: document.getElementById('name').value})
            .then(data => {if ((document.getElementById("nickname").value.trim().length >= 4) || document.getElementById("imagePreview").style !== document.getElementById("avatar").style)
                              window.location.href = window.location.href;})
 
