91b54f56cd5aa860c78de2acb77ab31c62234e96
Krokochik <pasha09braginec@gmail.com>
2023-01-23 00:53:56 +0300

    commit

--- src/main/java/com/krokochik/ideasForum/config/WebSocketConfiguration.java
+++ src/main/java/com/krokochik/ideasForum/config/WebSocketConfiguration.java
@@ -1,17 +1,11 @@
 package com.krokochik.ideasForum.config;
 
-import com.krokochik.ideasForum.controller.MFAEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 public class WebSocketConfiguration  {
-
-    @Bean
-    public MFAEndpoint mfaEndpoint() {
-        return new MFAEndpoint();
-    }
 
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
