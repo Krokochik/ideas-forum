402b8ec6273d0ff0f83961c911269c1d9e66606c
Krokochik <pasha09braginec@gmail.com>
2023-02-15 20:01:21 +0300

    commit

--- src/main/java/com/krokochik/ideasForum/controller/MFAEndpoint.java
+++ src/main/java/com/krokochik/ideasForum/controller/MFAEndpoint.java
@@ -1,7 +1,6 @@
 package com.krokochik.ideasForum.controller;
 
 import com.krokochik.ideasForum.config.CustomSpringConfigurator;
-import com.krokochik.ideasForum.model.CallbackTask;
 import com.krokochik.ideasForum.model.Message;
 import com.krokochik.ideasForum.repository.UserRepository;
 import com.krokochik.ideasForum.res.AESKeys;
@@ -10,14 +9,15 @@ import com.krokochik.ideasForum.service.crypto.TokenService;
 import com.krokochik.ideasForum.service.mfa.MessageDecoder;
 import com.krokochik.ideasForum.service.mfa.MessageEncoder;
 import com.krokochik.ideasForum.service.mfa.StorageService;
-import com.nimbusds.srp6.*;
+import com.nimbusds.srp6.SRP6CryptoParams;
+import com.nimbusds.srp6.SRP6Exception;
+import com.nimbusds.srp6.SRP6ServerSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 @ServerEndpoint(
@@ -31,30 +31,19 @@ public class MFAEndpoint {
     @Autowired
     UserRepository userRepo;
 
-    StorageService<String, ArrayList<CallbackTask<Message>>> onMessageTasksStorage = new StorageService<>();
-    StorageService<Session, String> sessionKeyStorage = new StorageService<>();
-    HashMap<Session, SRP6ServerSession> serverSessions = new HashMap<>();
-    HashMap<Session, BigInteger> B = new HashMap<>();
-    HashMap<Session, BigInteger> salts = new HashMap<>();
-    HashMap<Session, String> logins = new HashMap<>();
-    StorageService<Session, MessageCipher> messageCipherStorage = new StorageService<>();
+    StorageService<String, Object> sessionStorage = new StorageService<>();
     SRP6CryptoParams params = SRP6CryptoParams.getInstance(2048, "SHA-512");
+    MessageCipher cipher = new MessageCipher("username", "keyId", "ivId");
 
     @OnOpen
     public void onOpen(Session session) {
-        onMessageTasksStorage.save(session, "onMessage", new ArrayList<>());
     }
 
     @OnMessage
     public void onMessage(Session session, Message message) {
         if ((message.getContent().get("msg") == null) || !message.getContent().get("msg").equals("ping")) {
             System.out.println(message);
-            new Thread(() -> onMessageTasksStorage.get(session, "onMessage").forEach(task -> {
-                try {
-                    task.run(message);
-                } catch (Exception ignored) {
-                }
-            })).start();
+
             if ((message.getContent().size() == 1) && message.getContent().containsKey("username"))
                 authenticateStepOne(message.get("username"), session);
             if (message.getContent().containsKey("A") && message.getContent().containsKey("M1"))
@@ -63,17 +52,16 @@ public class MFAEndpoint {
 
     }
 
-    private void authenticateStepOne(String login, Session session) {
+    private void authenticateStepOne(String username, Session session) {
         new Thread(() -> {
             System.out.println("auth");
             SRP6ServerSession serverSession = new SRP6ServerSession(params);
-            BigInteger salt = new BigInteger(userRepo.findByUsername(login).getSalt());
-            BigInteger B = serverSession.step1(login, salt,
-                    new BigInteger(userRepo.findByUsername(login).getVerifier()));
-            serverSessions.put(session, serverSession);
-            this.B.put(session, B);
-            salts.put(session, salt);
-            logins.put(session, login);
+            BigInteger salt = new BigInteger(userRepo.findByUsername(username).getSalt());
+            BigInteger B = serverSession.step1(username, salt,
+                    new BigInteger(userRepo.findByUsername(username).getVerifier()));
+
+            sessionStorage.save(session, "serverSession", serverSession);
+            sessionStorage.save(session, "username", username);
 
             session.getAsyncRemote().sendObject(new Message(new HashMap<>(){{
                 put("B", B.toString());
@@ -85,28 +73,16 @@ public class MFAEndpoint {
 
     private void authenticateStepTwo(String A, String M1, Session session) {
         System.out.println("auth 2");
-        SRP6ServerSession serverSession = serverSessions.get(session);
-        String login = logins.get(session);
-        BigInteger salt = salts.get(session);
-        BigInteger B = this.B.get(session);
+        SRP6ServerSession serverSession = (SRP6ServerSession) sessionStorage.get(session, "serverSession");
 
-        if ((login != null) && (serverSession != null) && (salt != null) && (B != null)) {
+        if (serverSession != null) {
             System.out.println("step 2");
             try {
-                SRP6ClientSession clientSession = new SRP6ClientSession();
-                clientSession.step1(login, userRepo.findByUsername(login).getPassword());
-                SRP6ClientCredentials credentials = clientSession.step2(params, salts.get(session), this.B.get(session));
-                System.out.println(credentials.A);
-                System.out.println(credentials.M1);
-
                 serverSession.step2(new BigInteger(A), new BigInteger(M1));
 
                 String sessionKey = serverSession.getSessionKey().toString(16);
                 System.out.println(sessionKey);
-                sessionKeyStorage.save(session, sessionKey);
-
-                MessageCipher cipher = new MessageCipher("username", "keyId", "ivId");
-                messageCipherStorage.save(session, cipher);
+                sessionStorage.save(session, "sessionKey", sessionKey);
 
                 int keyId = (int) Math.floor(Math.random() * AESKeys.keys.length);
                 int ivId = (int) Math.floor(Math.random() * AESKeys.keys.length);
@@ -117,9 +93,11 @@ public class MFAEndpoint {
                     put("ivId", ivId + "");
                     put("keyId", keyId + "");
                 }});
+
+                String username = (String) sessionStorage.get(session, "username");
                 session.getAsyncRemote().sendObject(cipher.encrypt(response,
-                        TokenService.getHash(login + sessionKey, AESKeys.keys[ivId]),
-                        TokenService.getHash(login + sessionKey, AESKeys.keys[keyId])));
+                        TokenService.getHash(username + sessionKey, AESKeys.keys[ivId]),
+                        TokenService.getHash(username + sessionKey, AESKeys.keys[keyId])));
                 System.out.println("sent 2");
             } catch (SRP6Exception e) {
                 e.printStackTrace();
