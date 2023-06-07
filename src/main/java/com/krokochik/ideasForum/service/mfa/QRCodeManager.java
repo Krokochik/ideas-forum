b84587b4177b86ace9a51ee68b766c944dc56434
Krokochik <pasha09braginec@gmail.com>
2023-06-07 06:12:11 +0300

    commit

--- src/main/java/com/krokochik/ideasForum/service/mfa/QRCodeManager.java
+++ src/main/java/com/krokochik/ideasForum/service/mfa/QRCodeManager.java
@@ -46,8 +46,7 @@ public class QRCodeManager {
         BufferedImage qrCodeImage = generateQRCodeImage(content, size);
         BufferedImage roundedImage = roundCorners(qrCodeImage, cornerRadius);
 
-        File outputFile = new File("src/main/resources/dynamic/qr/" + filename + ".png"); // Укажите путь и имя файла для сохранения
-        System.out.println(System.getProperty("user.dir"));
+        File outputFile = new File(System.getProperty("java.io.tmpdir") + filename + ".png");
         ImageIO.write(roundedImage, "png", outputFile);
         savedQrCodes.put(username, filename);
     }
