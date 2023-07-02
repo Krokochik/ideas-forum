95ab92e0bd0aa1b1efd3513baa5c12796a1254cf
Krokochik <pasha09braginec@gmail.com>
2023-07-03 02:32:46 +0300

    commit

--- src/main/resources/static/scripts/captcha.js
+++ src/main/resources/static/scripts/captcha.js
@@ -4,15 +4,24 @@ $(document).ready(function() {
             $(this).prop('disabled', true);
             hcaptcha.execute();
             $('#submit').prop('disabled', true);
+            $('#prnt').addClass('translucent-after');
+            $('#prnt').removeClass('visible-after');
+            $('#prnt').removeClass('hidden-after');
         }
     });
 });
 function onSubmit(token) {
     $('#captchaLbl').css('background-color', '#ffeba7');
+    $('#prnt').addClass('visible-after');
+    $('#prnt').removeClass('translucent-after');
+    $('#prnt').removeClass('hidden-after');
 }
 function onError() {
     $('#squaredFour1').prop('disabled', false);
     $('#squaredFour1').prop('checked', false);
     $('#captchaLbl').css('background-color', '#ccc');
+    $('#prnt').addClass('hidden-after');
+    $('#prnt').removeClass('translucent-after');
+    $('#prnt').removeClass('visible-after');
     $('#submit').prop('disabled', false);
 }
