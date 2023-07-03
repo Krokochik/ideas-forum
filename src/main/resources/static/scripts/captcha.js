394f9bddb53076311d8bcf300c8b044950403394
Krokochik <pasha09braginec@gmail.com>
2023-07-03 03:04:55 +0300

    commit

--- src/main/resources/static/scripts/captcha.js
+++ src/main/resources/static/scripts/captcha.js
@@ -1,6 +1,8 @@
 var captchaPassed = false;
 
 $(document).ready(function() {
+    $('#submit').css({'color': '#102770!important', 'background-color' : '#ffeba7!important'})
+    $('#submit').prop('disabled', true);
     $('#captchaLbl').hover(function(){
         if (captchaPassed == false) {
             $('#prnt').addClass('translucent-after');
@@ -18,8 +20,8 @@ $(document).ready(function() {
         if ($(this).is(':checked')) {
             $(this).prop('disabled', true);
             hcaptcha.execute();
-            $('#submit').prop('disabled', true);
             captchaPassed = true;
+            $('#submit').prop('disabled', true);
             $('#prnt').addClass('translucent-after');
             $('#prnt').removeClass('visible-after');
             $('#prnt').removeClass('hidden-after');
@@ -27,19 +29,20 @@ $(document).ready(function() {
     });
 });
 function onSubmit(token) {
-    captchaPassed = true
-    $('#captchaLbl').css('background-color', '#ffeba7');
+    captchaPassed = true;
+    $('#captchaLbl').css({'background-color': '#ffeba7', 'pointer-events: none'});
+    $('#submit').prop('disabled', false);
     $('#prnt').addClass('visible-after');
     $('#prnt').removeClass('translucent-after');
     $('#prnt').removeClass('hidden-after');
 }
 function onError() {
-    captchaPassed = false
+    captchaPassed = false;
     $('#squaredFour1').prop('disabled', false);
     $('#squaredFour1').prop('checked', false);
     $('#captchaLbl').css('background-color', '#ccc');
     $('#prnt').addClass('hidden-after');
     $('#prnt').removeClass('translucent-after');
     $('#prnt').removeClass('visible-after');
-    $('#submit').prop('disabled', false);
+    $('#submit').prop('disabled', true);
 }
