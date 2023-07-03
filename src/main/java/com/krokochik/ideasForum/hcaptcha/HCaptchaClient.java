ab5c5f2cde0bbaa5b61cae838baea4f585688033
Krokochik <pasha09braginec@gmail.com>
2023-07-03 05:40:49 +0300

    commit

--- src/main/java/com/krokochik/ideasForum/hcaptcha/HCaptchaClient.java
+++ src/main/java/com/krokochik/ideasForum/hcaptcha/HCaptchaClient.java
@@ -1,33 +1,61 @@
 package com.krokochik.ideasForum.hcaptcha;
 
 import com.google.gson.Gson;
-import org.codehaus.jackson.map.ObjectMapper;
+import com.google.gson.JsonSyntaxException;
+import lombok.SneakyThrows;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
 
 public class HCaptchaClient {
     public static final String HCAPTCHA_API_ENDPOINT = "https://hcaptcha.com/siteverify";
     private static final Gson gson = new Gson();
 
+    @SneakyThrows
+    private static HCaptchaResponse sendRequest(HCaptchaRequest request) {
+        URL requestUrl = new URL(HCAPTCHA_API_ENDPOINT);
+        HttpsURLConnection connection = ((HttpsURLConnection) requestUrl.openConnection());
+
+        HCaptchaResponse response;
+
+        connection.setRequestMethod("POST");
+
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            OutputStream outputStream = connection.getOutputStream();
+            outputStream.write(("secret=" + request.secret + "&response=" + request.response + "&sitekey=" + request.sitekey).getBytes(StandardCharsets.UTF_8));
+            outputStream.flush();
+            outputStream.close();
+
+
+        // read response
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder responseBody = new StringBuilder();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            responseBody.append(line);
+        } reader.close();
+
+        System.out.println(responseBody);
+        try {
+            response = gson.fromJson(responseBody.toString(), HCaptchaResponse.class);
+        } catch (JsonSyntaxException e) {
+            throw new Exception("Unexpected server answer.");
+        }
+
+        return response;
+    }
+
+
     public static HCaptchaResponse verify(String secret, String response, String sitekey) throws IOException, InterruptedException {
         HCaptchaRequest hCaptchaRequest = new HCaptchaRequest(secret, response, sitekey);
-        Map<String, Object> map = new ObjectMapper().readValue(gson.toJson(hCaptchaRequest), Map.class);
-        String requestBody = URLEncoder.encode(map.toString(), StandardCharsets.UTF_8);
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(HCAPTCHA_API_ENDPOINT))
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .method("POST", HttpRequest.BodyPublishers.ofString(requestBody))
-                .build();
-        System.out.println(request.bodyPublisher().get());
-        HttpResponse<String> httpResponse = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
-        return gson.fromJson(httpResponse.body(), HCaptchaResponse.class);
+        return sendRequest(hCaptchaRequest);
     }
 }
