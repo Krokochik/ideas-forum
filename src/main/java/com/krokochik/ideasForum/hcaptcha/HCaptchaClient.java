5a7aadc26f7b1bf76d1deb1de9b777ffe79093f1
Krokochik <pasha09braginec@gmail.com>
2023-07-03 05:24:01 +0300

    commit

--- src/main/java/com/krokochik/ideasForum/hcaptcha/HCaptchaClient.java
+++ src/main/java/com/krokochik/ideasForum/hcaptcha/HCaptchaClient.java
@@ -1,12 +1,16 @@
 package com.krokochik.ideasForum.hcaptcha;
 
 import com.google.gson.Gson;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 public class HCaptchaClient {
     public static final String HCAPTCHA_API_ENDPOINT = "https://hcaptcha.com/siteverify";
@@ -14,11 +18,12 @@ public class HCaptchaClient {
 
     public static HCaptchaResponse verify(String secret, String response, String sitekey) throws IOException, InterruptedException {
         HCaptchaRequest hCaptchaRequest = new HCaptchaRequest(secret, response, sitekey);
-        String json = gson.toJson(hCaptchaRequest);
+        Map<String, Object> map = new ObjectMapper().readValue(gson.toJson(hCaptchaRequest), Map.class);
+        String requestBody = URLEncoder.encode(map.toString(), StandardCharsets.UTF_8);
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(HCAPTCHA_API_ENDPOINT))
-                .header("Content-Type", "application/json")
-                .method("POST", HttpRequest.BodyPublishers.ofString(json))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .method("POST", HttpRequest.BodyPublishers.ofString(requestBody))
                 .build();
         HttpResponse<String> httpResponse = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(httpResponse.body());
