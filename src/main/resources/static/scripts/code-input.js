5dbc9f842d0966cf3399f0114c32d53eb8ccd00a
Krokochik <pasha09braginec@gmail.com>
2023-07-31 22:00:41 +0300

    commit

--- src/main/resources/static/scripts/code-input.js
+++ src/main/resources/static/scripts/code-input.js
@@ -1,162 +1,198 @@
 class TwoFactorCode {
-  constructor(root, numberOfInputs) {
-    this.root = root;
-    this.numberOfInputs = numberOfInputs;
-    this.code = new Array(numberOfInputs).fill("");
-    this.inputs = undefined;
-    this.form = undefined;
+    constructor(root) {
+        this.root = root;
+        this.numberOfInputs =
+            root.dataset.fields != undefined
+                ? parseInt(root.dataset.fields, 10)
+                : 4;
+        this.color =
+            root.dataset.color != undefined ? root.dataset.color : "#000";
+        this.style = root.dataset.style != undefined ? root.dataset.style : "";
+        this.autoSubmit = root.dataset.submit !== "btn";
+        this.code = Array(this.numberOfInputs).fill("");
+        this.inputs = undefined;
+        this.form = undefined;
+        this.editable = true;
 
-    this.onInput = this.onInput.bind(this);
-    this.onKeydown = this.onKeydown.bind(this);
-    this.onInvalid = this.onInvalid.bind(this);
-  }
+        this.onInput = this.onInput.bind(this);
+        this.onKeydown = this.onKeydown.bind(this);
+        this.onFocus = this.onFocus.bind(this);
+        this.onInvalid = this.onInvalid.bind(this);
+    }
 
-  generateHTML() {
-    const inputs = new Array(this.numberOfInputs)
-      .fill(undefined)
-      .map((input, index) => {
-        return `<input
-                data-index="${index}"
-                aria-label="input for code at position ${index}"
-                type="text"
-                required
-                />`;
-      });
+    generateHTML() {
+        if (this.style == "") this.style = "color:" + this.color;
+        else if (!this.style.includes("color:"))
+            this.style += ";color:" + this.color;
+        const inputs = Array.from(
+            { length: this.numberOfInputs },
+            (_, index) =>
+                `<input
+              data-index="${index}"
+              aria-label="input for code at position ${index}"
+              type="text"
+              title=""
+              style="${this.style}"
+              required
+             />`
+        )
+            .join("")
+            .trim();
 
-    return `
-      <form>
-        <div class="two-factor-code-inputs">${inputs.join("").trim()}</div>
-      </form>
-    `;
-  }
+        if (this.autoSubmit)
+            return `
+             <form>
+                <div class="two-factor-code-inputs">${inputs}</div>
+             </form>`;
+        else {
+            return `
+            <form>
+                <div class="two-factor-code-inputs">${inputs}</div>
+                <button type="submit" class="submit-btn">Подтвердить<button/>
+             </form>`;
+        }
+    }
 
-  render() {
-    this.container = document.createElement("div");
-    this.container.classList.add("two-factor-code");
-    this.container.innerHTML = this.generateHTML();
+    render() {
+        this.container = document.createElement("div");
+        this.container.classList.add("two-factor-code");
+        this.container.innerHTML = this.generateHTML();
 
-    this.inputs = this.container.querySelectorAll(".two-factor-code input");
-    this.form = this.container.querySelector("form");
+        this.inputs = this.container.querySelectorAll(".two-factor-code input");
+        this.form = this.container.querySelector("form");
 
-    this.root.appendChild(this.container);
+        this.root.appendChild(this.container);
 
-    this.addEventListeners();
-  }
+        this.addEventListeners();
+    }
 
-  addEventListeners() {
-    this.container.addEventListener("input", this.onInput);
-
-    this.container.addEventListener("keydown", this.onKeydown);
-
-    this.inputs.forEach((input) => {
-      input.addEventListener("invalid", this.onInvalid);
-      input.style.color = this.root.dataset.color;
-    });
-  }
-
-    submitCode() {
-      // since we're using the button as a submit
-      // we don't want to actually submit the form when it's possible
-      // use checkValidity() to see if we have all the inputs filled out so that we can preventDefault the submit
-      if (this.form.checkValidity()) {
-        console.log(this.code);
+    addEventListeners() {
+        this.container.addEventListener("input", this.onInput);
+        this.container.addEventListener("keydown", this.onKeydown);
 
         this.inputs.forEach((input) => {
-            input.blur();
-            input.readOnly = true;
+            input.addEventListener("invalid", this.onInvalid);
+            input.addEventListener("focus", (event) => this.onFocus(event));
         });
+    }
 
-        if (this.code.join("") === "1234") {
+    submitCode() {
+        if (this.form.checkValidity()) {
+            console.log(this.code);
 
-        } else {
+            this.editable = false;
             this.inputs.forEach((input) => {
-                input.value = '';
+                input.blur();
+                input.readOnly = true;
             });
-            this.inputs.forEach((input) => {
-                input.dispatchEvent(new Event("invalid"));
-                input.readOnly = false;
-            });
+
+            const isCodeRight = (code) => {
+                // Handle code here
+                return code.join("") === "1234";
+            };
+
+            const badCode = () => {
+                // Handle the incorrect code case here
+            };
+
+            if (isCodeRight(this.code)) {
+                // Handle the correct code case here
+            } else {
+                badCode();
+                this.editable = true;
+                this.inputs.forEach((input) => {
+                    input.value = "";
+                });
+                this.inputs.forEach((input) => {
+                    input.dispatchEvent(new Event("invalid"));
+                    input.readOnly = false;
+                });
+                this.inputs[0].focus();
+            }
         }
-      }
     }
 
-  onInput(event) {
-    let value = event.target.value.replace(/\s+/g, "").toUpperCase();
-    let currentIndex = Number(event.target.dataset.index);
+    onInput(event) {
+        if (this.editable) {
+            let value = event.target.value.replace(/\s+/g, "").toUpperCase();
+            let currentIndex = Number(event.target.dataset.index);
 
-    const digitsRegex = /^\d*$/;
-    const lettersRegex = /^[A-Za-z]+$/;
-    const lettersAndDigitsRegex = /^[A-Za-z0-9]+$/;
+            const digitsRegex = /^\d*$/;
+            const lettersRegex = /^[A-Za-z]+$/;
+            const lettersAndDigitsRegex = /^[A-Za-z0-9]+$/;
 
-    var regex = undefined;
-    if ("filter" in this.root.dataset && this.root.dataset.filter.includes("+")) {
-        const filter = this.root.dataset.filter.split("+");
-        if (filter.includes("num"))
-            if (filter.includes("ltr"))
-                regex = lettersAndDigitsRegex;
-            else regex = digitsRegex;
-        else if (filter.includes("ltr"))
-            regex = lettersRegex;
-    } else regex = digitsRegex
+            const filter = this.root.dataset.filter?.split("+") || [];
+            const regex = filter.includes("num")
+                ? filter.includes("ltr")
+                    ? lettersAndDigitsRegex
+                    : digitsRegex
+                : filter.includes("ltr")
+                ? lettersRegex
+                : digitsRegex;
 
-    if (!regex.test(value)) {
-      // can't prevent input from not being saved in the value cause this event doesn't handle that
-      // preventDefault() also doesn't do anything on event 'input',
-      // so just set the value to the old value
-      event.target.value = this.code[currentIndex];
-      return;
+            if (!regex.test(value)) {
+                event.target.value = this.code[currentIndex];
+                return;
+            }
+
+            while (value.length > 0 && currentIndex < this.numberOfInputs) {
+                this.inputs[currentIndex].value = value.slice(0, 1);
+                this.code[currentIndex] = value.slice(0, 1);
+
+                const nextIndex = ++currentIndex;
+
+                if (nextIndex < this.numberOfInputs && value.slice(1) !== "") {
+                    const nextDigit = value.slice(1);
+                    this.code[nextIndex] = nextDigit;
+
+                    const nextInput = this.inputs[nextIndex];
+                    nextInput.focus();
+                    nextInput.value = nextDigit;
+
+                    value = value.slice(1);
+                } else {
+                    value = "";
+                }
+            }
+
+            if (
+                this.code.filter((str) => str.trim() !== "").length ===
+                this.numberOfInputs
+            ) {
+                this.submitCode();
+            }
+        }
     }
 
-    while (value.length > 0 && currentIndex < this.numberOfInputs) {
-      this.inputs[currentIndex].value = value.slice(0, 1);
-      this.code[currentIndex] = value.slice(0, 1);
-
-      const nextIndex = ++currentIndex;
-
-      if (nextIndex < this.numberOfInputs && value.slice(1) !== '') {
-        const nextDigit = value.slice(1);
-        console.log(nextDigit)
-        this.code[nextIndex] = nextDigit;
-
-        const nextInput = this.inputs[nextIndex];
-        nextInput.focus();
-        nextInput.value = nextDigit;
-
-        value = value.slice(1);
-      } else value = '';
+    onKeydown(event) {
+        if (event.code === "Backspace" && this.editable) {
+            event.preventDefault();
+            const currentIndex = Number(event.target.dataset.index);
+            const prevIndex =
+                currentIndex > 0 ? currentIndex - 1 : currentIndex;
+            this.inputs[prevIndex].focus();
+            this.inputs[currentIndex].value = "";
+            this.code[currentIndex] = "";
+        }
     }
 
-    if (this.code.filter((str) => str.trim() !== "").length == this.numberOfInputs)
-        this.submitCode();
-
-    // console.log(this.code);
-  }
-
-  onKeydown(event) {
-    if (event.code === "Backspace") {
-      event.preventDefault();
-      const currentIndex = Number(event.target.dataset.index);
-      var prevIndex = 0;
-      if (currentIndex > 0)
-          prevIndex = currentIndex - 1;
-      else prevIndex = currentIndex;
-      this.inputs[prevIndex].focus();
-      this.inputs[currentIndex].value = "";
-      this.code[currentIndex] = "";
+    onFocus(event) {
+        if (!this.editable) {
+            event.preventDefault();
+            event.target.blur();
+        }
     }
-  }
 
-  // add this event handler on each the inputs to add a class that will handle highlighting the
-  // input borders that are empty
-  onInvalid(event) {
-    this.container
-      .querySelector(".two-factor-code-inputs")
-      .classList.add("submitted");
-  }
+    onInvalid(event) {
+        this.container
+            .querySelector(".two-factor-code-inputs")
+            .classList.add("submitted");
+    }
 }
 
 function render() {
     const root = document.getElementById("code-input");
-    const code = new TwoFactorCode(root, parseInt(root.dataset.fields));
+    root.innerHTML = "";
+    const code = new TwoFactorCode(root);
     code.render();
 }
