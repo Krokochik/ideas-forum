87fd672c95c8b47d3ca91478e454ea637a52b9a8
Krokochik <pasha09braginec@gmail.com>
2023-08-01 04:25:08 +0300

    commit

--- src/main/resources/static/scripts/code-input.js
+++ src/main/resources/static/scripts/code-input.js
@@ -87,7 +87,7 @@ class TwoFactorCode {
             });
 
             const changeChildrenVisibility = () => {
-                let children = document.getElementById("modal-neck").childNodes;
+                let children = document.getElementById("modal-neck").children;
                 children.forEach(child => {
                     child.style.display = child.style.display === "none" ? "block" : "none";
                 });
