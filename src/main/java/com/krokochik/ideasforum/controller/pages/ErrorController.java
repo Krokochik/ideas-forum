2c09f004afc8e279f9c6ee1b33c1f35a0f580a29
Krokochik <pasha09braginec@gmail.com>
2023-08-06 02:41:00 +0300

    commit

--- src/main/java/com/krokochik/ideasforum/controller/pages/ErrorController.java
+++ src/main/java/com/krokochik/ideasforum/controller/pages/ErrorController.java
@@ -1,30 +1,29 @@
-package com.krokochik.ideasforum.controller.pages;
-
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
-
-    @GetMapping("/error")
-    public String handleError(HttpServletRequest request, Model model) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        request.getAttributeNames().asIterator().forEachRemaining(string -> System.out.println(string + ": " + request.getAttribute(string)));
-
-        if (status != null) {
-            int statusCode = Integer.parseInt(status.toString());
-
-            if (statusCode == 403)
-                return "redirect:/email-validity-confirmation";
-
-            model.addAttribute("code", statusCode + "");
-            model.addAttribute("message", "Error " + statusCode + " : "
-                    +  HttpStatus.valueOf(statusCode).getReasonPhrase());
-        }
-        return "error";
-    }
-}
+//package com.krokochik.ideasforum.controller.pages;
+//
+//import jakarta.servlet.RequestDispatcher;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+//
+//    @GetMapping("/error")
+//    public String handleError(HttpServletRequest request, Model model) {
+//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//
+//        if (status != null) {
+//            int statusCode = Integer.parseInt(status.toString());
+//
+//            if (statusCode == 403)
+//                return "redirect:/email-validity-confirmation";
+//
+//            model.addAttribute("code", statusCode + "");
+//            model.addAttribute("message", "Error " + statusCode + " : "
+//                    +  HttpStatus.valueOf(statusCode).getReasonPhrase());
+//        }
+//        return "error";
+//    }
+//}
